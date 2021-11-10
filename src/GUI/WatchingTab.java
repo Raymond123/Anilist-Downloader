59d9efb89cc3cdb2865963c7e5b4516f22fd2749
eastonsmith <eastonsmith37@gmail.com>
2021-11-09 23:51:02 -0500

    finished creating buttons for opening show ep links. and linked the ep buttons to each appropriate magnet links.

--- src/GUI/WatchingTab.java
+++ src/GUI/WatchingTab.java
@@ -4,11 +4,17 @@ import APIcon.Post;
 import APIcon.Titles;
 
 import javax.annotation.processing.FilerException;
+import javax.print.URIException;
 import javax.swing.*;
 import javax.xml.crypto.NodeSetData;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.*;
+import java.util.List;
 
 public class WatchingTab extends JPanel {
 
@@ -64,29 +70,37 @@ public class WatchingTab extends JPanel {
         this.t = titles;
         try{
             this.titles = new LinkedList<>();
-            magnetLinks = getLinks();
+            creatButtons(getLinks());
         }catch(IOException io){
             io.printStackTrace();
-        }
-
-        for(List<String> show : magnetLinks){
-            int epNum=1;
-            for(String link : show){
-                System.out.println("Epsiode " + epNum++ + " " + link);
-            }
-            System.out.println();
         }
     }
 
     private void creatButtons(List<List<String>> shows){
         Node<String> t = this.titles.getFirst();
         for(List<String> links : shows){
-
+            if(t==null) break;
             JButton showBut = new JButton(t.getElement());
             t = t.getNext();
-
             this.actionListener = e -> {
-
+                int epNum = 1;
+                JFrame linkButFrame = new JFrame();
+                linkButFrame.setLayout(new GridLayout());
+                for(String link : links){
+                    JButton linkBut = new JButton("episode " + epNum++);
+                    linkBut.addActionListener(e1 -> {
+                        try {
+                            //if (openWebpage(new URI(link))) JOptionPane.showMessageDialog(linkButFrame, "Link opened");
+                            openWebpage(new URI(link));
+                        }catch(URISyntaxException uri){
+                            uri.printStackTrace();
+                        }
+                    });
+                    linkButFrame.add(linkBut);
+                    linkButFrame.setSize(800, 600);
+                    linkButFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                    linkButFrame.setVisible(true);
+                }
             };
             showBut.addActionListener(this.actionListener);
             this.add(showBut);
@@ -113,30 +127,42 @@ public class WatchingTab extends JPanel {
                 magLinkArr = new ArrayList<>();
 
                 Node<String> node = new TNode<>(line);
-                titles.getLast().setNext(node);
-                titles.add(node);
-
+                if(this.titles.size()>=1) {
+                    System.out.println(this.titles.getLast().getElement());
+                    this.titles.getLast().setNext(node);
+                }
+                this.titles.add(node);
                 showObjs.add(magLinkArr);
                 mag = true;
                 continue;
-
             }else if(line.equals("")){
-
                 int s = magLinks.size();
                 for(int i=0; i<s; i++){
                     magLinkArr.add(magLinks.pop());
                 }
                 mag = false;
-
             }
             if(mag && !(line.equals(": "))){
                 //System.out.println("link: " + line);
                 magLinks.push(line);
             }
         }
-
         return showObjs; //placeholder return
     }
+
+    private static boolean openWebpage(URI uri) {
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
+                desktop.browse(uri);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
 
     private List<String> simplifyList(List<String> arrayList) {
         ArrayList<String> ret = new ArrayList<>();
